6bbe0c6cd5c2907f7bdb0e46615dde37ae6c760f
Sean <toranos@gmail.com>
2015-08-16 21:12:10 +1000

    Fixed missing braces/brackets and Init logic
    Added missing braces/brackets. Also switched from != to == for checking
    if vlad had already been allocated memory since it was backwards before.

--- allocator.c
+++ allocator.c
@@ -90,10 +90,11 @@ void vlad_init(u_int32_t size)
    // memory_size = 0;
    // TODO
    // remove the above when you implement your code
-
-   if (memory != NULL){ // if already initialised, do nothing
+   printf("0\n"); // debug
+   if (memory == NULL){ // if already initialised, do nothing
       size = power2(size); // translate to smallest larger power of 2
       memory = malloc(size); // malloc returns NULL on fail
+      printf("b\n"); // debug
       if (memory == NULL){   // if malloc failed:
          fprintf(stderr, "vlad_init: insufficient memory");
          abort();
@@ -105,6 +106,7 @@ void vlad_init(u_int32_t size)
       init_header->size = size;
       init_header->next = free_list_ptr;
       init_header->prev = free_list_ptr;
+      printf("a\n"); // debug
    }
 }
 
@@ -134,13 +136,15 @@ void *vlad_malloc(u_int32_t n)
 
    // NOTE: Need to search for smallest region BEFORE splitting regions.
    while (done == 0){ // search for smallest region that can fit n
-      if ((curr->size >= HEADER_SIZE + n) && ((curr->size < chosen_size) || (chosen_size == 0)){
+      printf("1\n"); // debug
+      if ((curr->size >= HEADER_SIZE + n) && ((curr->size < chosen_size) || (chosen_size == 0))){
          chosen = curr;
          chosen_size = curr->size;
       }
       if (curr->next == free_list_ptr){
          done = 1; // finished search
       }
+      printf("2\n"); // debug
       curr = itop(curr->next); // move to next region
    }
    // OLD UNFINISHED METHOD:
@@ -164,13 +168,13 @@ void *vlad_malloc(u_int32_t n)
    //    }
    // }
 
-
+   printf("3\n"); // debug
    // NOTE: next and prev are not real pointers but indexes!
    // void *, vaddr_t, vlink_t refer to locations in memory[]
    // need a way to map vetween void * and vaddr_t (i.e. pointer & index)
    byte *new_addr; // used for pointer arithmetic
    free_header_t *new;
-   if ((curr->size/2) >= (HEADER_SIZE + n){ // if can fit in half
+   if ((curr->size/2) >= (HEADER_SIZE + n)){ // if can fit in half
       // split region into 2
       new_addr = (byte *) curr + (curr->size/2);
       new = (free_header_t *) new_addr;
